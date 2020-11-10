3fa3b5c7f446139146f264140dacd4eac9c70fa5
Cesar Sturion <csturion@gmail.com>
2020-11-10 09:25:52 -0300

    update

--- terraform/data.tf
+++ terraform/data.tf
@@ -3,17 +3,17 @@ data "azurerm_resource_group" "ops" {
   name = var.resource-group-ops
 }
 
-# data "azurerm_key_vault" "keyvault" {
-#   name                = var.keyvaul-name
-#   resource_group_name = data.azurerm_resource_group.ops.name
-# }
+data "azurerm_key_vault" "keyvault" {
+  name                = var.keyvaul-name
+  resource_group_name = data.azurerm_resource_group.ops.name
+}
 
-# data "azurerm_key_vault_secret" "dbpass-secret" {
-#   name         = "DBPassword${var.environment[terraform.workspace]}"
-#   key_vault_id = data.azurerm_key_vault.keyvault.id
-# }
+data "azurerm_key_vault_secret" "dbpass-secret" {
+  name         = "DBPassword${var.environment[terraform.workspace]}"
+  key_vault_id = data.azurerm_key_vault.keyvault.id
+}
 
-# data "azurerm_key_vault_secret" "backend" {
-#   name         = "KeyBackend"
-#   key_vault_id = data.azurerm_key_vault.keyvault.id
-# }
+data "azurerm_key_vault_secret" "backend" {
+  name         = "KeyBackend"
+  key_vault_id = data.azurerm_key_vault.keyvault.id
+}
